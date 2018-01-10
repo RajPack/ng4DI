afce8d42e6229309ebded72469581ec0a3e6fea8
RajPack <rckraja@hotmail.com>
2018-01-10 19:10:26 +0530

    sdf

--- app1/src/app/productSection/productSection.component.ts
+++ app1/src/app/productSection/productSection.component.ts
@@ -7,8 +7,9 @@ import { ProductListService } from '../services/productList.services';
 })
 export class ProductSectionComponent {
     private productList;
-    constructor(@Inject('ProductService') private productService) {
+    constructor( @Inject('ProductService') private productService) {
         this.productList = this.productService.getProductList();
+        this.productService.setProductList(['Monday', 'Tuesday', 'Wednesday']);
     }
 }
 
--- app1/src/app/services/productList.services.ts
+++ app1/src/app/services/productList.services.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductListService {
     private list: any[];
-    consturctor() {
+    constructor() {
         this.list = [1, 2, 3, 4, 5];
     }
     public getProductList() {
-        console.log(this);
+        console.log(this.list);
         return this.list;
     }
     public setProductList (list) {
