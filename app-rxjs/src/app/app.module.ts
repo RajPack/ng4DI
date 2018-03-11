337328197b872e0f536d49ec6d0faf1f00575ac7
RajPack <rckraja@hotmail.com>
2018-03-12 02:14:21 +0530

    app updated

--- app-rxjs/src/app/app.module.ts
+++ app-rxjs/src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { BlogService } from './blog/blog.service';
 
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './blogList/blogList.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    BlogService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
