33d69d44aaab9199429ba5d1ed254157deb270aa
RajPack <rckraja@hotmail.com>
2018-03-12 23:40:09 +0530

    updated

--- app-rxjs/src/app/blog/blog.service.ts
+++ app-rxjs/src/app/blog/blog.service.ts
@@ -1,13 +1,35 @@
 import { Blog, BlogEdit } from './blog.model';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { initialBloglist } from './blog.data';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
+
+
 
 export class BlogService {
     private blogListSubject: BehaviorSubject<Blog[]> = new BehaviorSubject(initialBloglist);
+
     getBlogListSubject(): BehaviorSubject<Blog[]> {
         return this.blogListSubject;
     }
     updateBlog(data: BlogEdit) {
-        console.log(data);
+        let currentList: Blog[], updatedBlog: Blog, matchIndex: number;
+        this.blogListSubject.subscribe((list) => {currentList = list; });
+        updatedBlog = currentList.filter((blog, index) => {
+            return blog.id === data.id ? (matchIndex = index) || true : false ;
+        }).map((blog) => {
+            return  {
+                id: blog.id,
+                title: data.title,
+                content: data.content,
+                thumbsDown: blog.thumbsDown,
+                thumbsUp: blog.thumbsUp,
+                author: data.author
+            };
+        })[0];
+        currentList[matchIndex] = updatedBlog;
+        this.blogListSubject.next(currentList);
     }
 }
