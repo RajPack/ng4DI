fec31d20d73cad99de6859dfd34feac59f11b173
RajPack <rckraja@hotmail.com>
2018-03-12 20:18:37 +0530

    updated

--- app-rxjs/src/app/blogEdit/blogEdit.component.ts
+++ app-rxjs/src/app/blogEdit/blogEdit.component.ts
@@ -1,4 +1,11 @@
 
-export class BlogEdit {
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-blog-edit',
+    templateUrl: './blogEdit.component.html',
+    styleUrls:  ['./blogEdit.component.css']
+})
+export class BlogEditComponent {
 
 }
